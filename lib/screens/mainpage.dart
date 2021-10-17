b9bc27c7f38204e73d34c1663237258861361b10
Asim Nasar Siddiqui <asim.ns@aphelia>
2021-10-17 18:53:20 +0530

    added mainpage tabs

--- lib/screens/mainpage.dart
+++ lib/screens/mainpage.dart
@@ -1,4 +1,10 @@
+import 'package:cab_driver/brand_colors.dart';
+import 'package:cab_driver/tabs/earnings_tab.dart';
+import 'package:cab_driver/tabs/home_tab.dart';
+import 'package:cab_driver/tabs/profile_tab.dart';
+import 'package:cab_driver/tabs/ratings_tab.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -7,24 +13,69 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
+  TabController tabController;
+  int selectedIndex = 0;
+
+  onItemClicked(int index) {
+    setState(() {
+      selectedIndex = index;
+      tabController.index = index;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Main Page"),
+      body: TabBarView(
+        controller: tabController,
+        physics: NeverScrollableScrollPhysics(),
+        children: [
+          HomeTab(),
+          EarningsTab(),
+          RatingsTab(),
+          ProfileTab(),
+        ],
       ),
-      body: Center(
-        child: MaterialButton(
-          child: Text("Hello"),
-          color: Colors.blue,
-          minWidth: 200,
-          onPressed: () {
-            DatabaseReference ref =
-                FirebaseDatabase.instance.reference().child('testing');
-            ref.set('testing connection');
-          },
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card),
+            label: "Earnings",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: "Ratings",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Account",
+          ),
+        ],
+        unselectedItemColor: BrandColors.colorIcon,
+        selectedItemColor: BrandColors.colorOrange,
+        showUnselectedLabels: true,
+        selectedLabelStyle: TextStyle(fontSize: 12),
+        type: BottomNavigationBarType.fixed,
+        onTap: onItemClicked,
+        currentIndex: selectedIndex,
       ),
     );
   }
